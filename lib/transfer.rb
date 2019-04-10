a251d87e40f2070f01fbf15c8d027e1e6aa993e9
Savannah C <sgcorrell@gmail.com>
2019-04-10 19:28:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,7 @@ class Transfer
     if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
+      self.status = "complete"
     end
   end
 end
