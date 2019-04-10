f473afafaba2460e15d58bc85cd8a0531d5f9bae
Savannah C <sgcorrell@gmail.com>
2019-04-10 19:20:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,9 @@ class Transfer
   attr_accessor :sender, :receiver, :status
   
   def initialize(sender, receiver)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
     
   end
 end
